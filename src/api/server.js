07a010d4a0d53653ffbd6cc09b5faee9d4ca1a79
ScriptCamilo <script.camilo@gmail.com>
2021-11-12 23:28:34 -0300

    style: Change server folder location

---
+++ src/api/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+const chatController = require('../controllers/chatController');
+
+io.on('connection', () => {
+  console.log('IO connected');
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', chatController);
+
+app.use((err, _req, res, _next) => {
+  const { status, message } = err;
+  const statusCode = status || 500;
+
+  res.status(statusCode).json({ message });
+});
+
+http.listen(PORT, () => console.log(`WebChat project running on port ${PORT}`));
