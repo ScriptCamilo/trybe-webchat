8ad56d371f732bad5e3b67a4917c0bbfef77e2be
ScriptCamilo <script.camilo@gmail.com>
2021-11-15 16:43:07 -0300

    feat: Complete the requirement two

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,15 +1,23 @@
 const socket = window.io('http://localhost:3000');
-const form = document.querySelector('#sendMessage');
-const messageInput = document.querySelector('#messageInput');
+const messagesForm = document.querySelector('#message-form');
+const messagesList = document.querySelector('#messages');
+const messageBox = document.querySelector('#message-box');
+const onlineUser = document.querySelector('#online-user');
+const nicknameForm = document.querySelector('#nickname-form');
+const nicknameBox = document.querySelector('#nickname-box');
 
-function listenMessages() {
-  socket.on('message', (message) => {
-    console.log(message);
-  });
+function getUser() {
+  const user = sessionStorage.getItem('user');
+  return user;
 }
 
-function clearInput() {
-  messageInput.value = '';
+function clearInput(input) {
+  const inputBox = input;
+  inputBox.value = '';
+}
+
+function setOnlineUser() {
+  onlineUser.innerText = getUser();
 }
 
 // https://gist.github.com/6174/6062387
@@ -17,30 +25,49 @@ function randomChar() {
   return Math.random().toString(36).substring(2, 10);
 }
 
-function saveRandomUser() {
+function saveUser(newUser) {
   const char = randomChar() + randomChar();
   const user = sessionStorage.getItem('user');
   if (!user) sessionStorage.setItem('user', char);
+  if (newUser) sessionStorage.setItem('user', newUser);
 }
 
-function getUser() {
-  const user = sessionStorage.getItem('user');
-  return user;
+function changeNickname(e) {
+  e.preventDefault();
+  const newUser = nicknameBox.value;
+  saveUser(newUser);
+  setOnlineUser();
+  clearInput(nicknameBox);
+}
+
+function createMessage(message) {
+  const messageList = document.createElement('li');
+  messageList.setAttribute('data-testid', 'message');
+  messageList.innerText = message;
+  messagesList.appendChild(messageList);
+}
+
+function listenMessages() {
+  socket.on('message', (message) => {
+    createMessage(message);
+  });
 }
 
 function sendMessage(e) {
   e.preventDefault();
   const message = {
-    chatMessage: messageInput.value,
+    chatMessage: messageBox.value,
     nickname: getUser(),
   };
-  clearInput();
+  clearInput(messageBox);
   socket.emit('message', message);
 }
 
-form.addEventListener('submit', sendMessage);
+messagesForm.addEventListener('submit', sendMessage);
+nicknameForm.addEventListener('submit', changeNickname);
 
 window.onload = () => {
-  saveRandomUser();
+  saveUser();
   listenMessages();
+  setOnlineUser();
 };
